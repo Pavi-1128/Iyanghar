d74cfff9f345df180dd28b4dfb84002d8488f011
Pavi-1128 <pavithra@v-accel.ai>
2025-11-27 13:20:33 +0530

    build

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import ServiceImg from "../asset/Serviceimg.jpg";
+import ServiceImg from "../asset/ServiceImg.jpg";
 import {
   Calculator,
   FileText,
